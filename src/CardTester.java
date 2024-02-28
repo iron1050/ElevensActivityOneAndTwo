e7519a931b19cbcf99fd3ebd01b85f6b07a289ea
Chukwuma Dibia <chukwumadibia@gmail.com>
2024-02-27 22:09:02 -0500

    Activity 1 and 2

--- src/CardTester.java
+++ src/CardTester.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class CardTester {
+/**
+ * This is a class that tests the Card class.
+ */
+public class CardTester {
+
+    /**
+     * The main method in this class checks the Card operations for consistency.
+     *	@param args is not used.
+     */
+    public static void main(String[] args) {
+        Card test1 = new Card("aura level 9999", "testsuit", 25);
+        System.out.println(test1);
+    }
 }
--- src/DeckTester.java
+++ src/DeckTester.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class DeskTester {
+/**
+ * This is a class that tests the Deck class.
+ */
+public class DeckTester {
+
+    /**
+     * The main method in this class checks the Deck operations for consistency.
+     *	@param args is not used.
+     */
+    public static void main(String[] args) {
+        String[] ranks = {"jack", "queen", "king"};
+        String[] suits = {"blue", "red"};
+        int[] pointValues = {11, 12, 13};
+        Deck d = new Deck(ranks, suits, pointValues);
+        System.out.println(d);
+        d.deal();
+        System.out.println(d);
+    }
 }
